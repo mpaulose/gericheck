ee463ed21ddac9283e0287032215d6750518c3bf
Paulose, Majo <Majo_Paulose@intuit.com>
2026-08-12 15:23:25 -0500

    removing tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'tailwindcss'; // Injects Tailwind CSS styles instantly
+// import 'tailwindcss'; // Injects Tailwind CSS styles instantly
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
